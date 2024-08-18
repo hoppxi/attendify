2f2006c9eb63aeb8e931581921b08cbd8d7a205f
Ermiyas Arage <152753390+ermi111@users.noreply.github.com>
2024-08-18 08:21:49 +0300

    Enable seletion for each chips in the Chips component

--- src/components/chips/Chips.tsx
+++ src/components/chips/Chips.tsx
@@ -1,59 +1,74 @@
-
-import React from "react";
+import React, { useState } from "react";
 import style from "./Chips.module.css";
-import Button from "../buttons/Buttons";
+import Button from "@/components/buttons/Buttons";
 
 type ChipsButtonProps = {
-    label: string,
-    selected?: boolean,
-    onClick?: () => {}
-}
+    label: string;
+    selected?: boolean;
+    onSelect?: () => void;
+};
 
 type ChipsTitle = {
-    icon?: React.ReactNode,
-    label: React.ReactNode
-}
+    icon?: React.ReactNode;
+    label: React.ReactNode;
+};
 
 type ChipsContainerProps = {
-    title: ChipsTitle,
-    buttons: Array<ChipsButtonProps>
-}
+    title: ChipsTitle;
+    buttons: ChipsButtonProps[];
+};
 
 interface ChipsProps {
-    chips: Array<ChipsContainerProps>
+    chips: ChipsContainerProps[];
 }
 
-const Chips: React.FC<ChipsProps> = ({chips}) => {
-    return(
+const Chips: React.FC<ChipsProps> = ({ chips }) => {
+    const [selectedChips, setSelectedChips] = useState<{ [key: number]: boolean }>({});
+
+    const handleClick = (containerIndex: number, buttonIndex: number, onClick?: () => void) => {
+        if (onClick) onClick();
+        
+        setSelectedChips(prevState => {
+            const newState = { ...prevState };
+            const key: number = containerIndex - buttonIndex;
+            newState[key] = !newState[key];
+            return newState;
+        });
+    };
+
+    return (
         <div className={style.chips_container}>
-            {
-                chips.map((container, i) => (
-                    <div key={i} className={style.chips_sections}>
-                        <div className={style.chips_title}>
-                            <span className="material-symbols-outlined">{container.title.icon}</span>
-                            <span className={style.text}>{container.title.label}</span>
-                        </div>
-                        <div className={style.chips_btn}>
-                            {
-                                container.buttons.map((button, i) => (
-                                    <Button 
-                                        key={i}
-                                        variant="outlined" 
-                                        onClick={button.onClick} 
-                                        className={
-                                            `${style.btn_chips} 
-                                            ${button.selected && style.btn_chips_selected}`}
-                                    >
-                                        {button.label}
-                                    </Button>
-                                ))
-                            }
-                        </div>
+            {chips.map((container, containerIndex) => (
+                <div key={containerIndex} className={style.chips_sections}>
+                    <div className={style.chips_title}>
+                        {container.title.icon && (
+                            <span className="material-symbols-outlined">
+                                {container.title.icon}
+                            </span>
+                        )}
+                        <span className={style.text}>{container.title.label}</span>
                     </div>
-                ))
-            }
+                    <div className={style.chips_btn}>
+                        {container.buttons.map((button, buttonIndex) => {
+                            const key: number = containerIndex - buttonIndex;
+                            return (
+                                <Button
+                                    key={buttonIndex}
+                                    variant="outlined"
+                                    onClick={() => handleClick(containerIndex, buttonIndex, button.onSelect)}
+                                    className={`${style.btn_chips} ${
+                                        selectedChips[key] ? style.btn_chips_selected : ""
+                                    }`}
+                                >
+                                    {button.label}
+                                </Button>
+                            );
+                        })}
+                    </div>
+                </div>
+            ))}
         </div>
-    )
-}
+    );
+};
 
 export default Chips;
