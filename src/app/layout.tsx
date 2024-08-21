3dd9e5dcf421fac30f008030a7aef7645d2f6b3a
Ermiyas Arage <152753390+ermi111@users.noreply.github.com>
2024-08-21 15:09:38 +0300

    Add page title for each route from layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
-
-import type { Metadata } from "next";
+"use client"
 import { NavDrawerProvider } from "@/context/NavDrawerContext";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import titleMap from "./page-title";
+import { usePathname } from "next/navigation";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "@fontsource/material-symbols-outlined";
 import "./globals.css";
 
-export const metadata: Metadata = {
-	title: "Attendify",
-	description: "App made for schools for attendanc management.",
-};
-
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+	const currentPath = usePathname();
+	const pageTitle: string = titleMap[currentPath];
+	const description: string = "Attendify is an innovative web app designed for efficient school attendance management. Featuring seamless QR code scanning for quick and accurate check-ins, Attendify provides real-time attendance tracking, automated report generation, and user-friendly dashboards for administrators and teachers. Enhance your school's attendance system with Attendify's robust features and streamlined interface.";
 	return (
 		<html lang="en">
+			<head>
+				<title>{pageTitle}</title>
+				<meta name="description" content={description} />
+			</head>
 			<body>
 				<NavDrawerProvider>
 					{children}
---
+++ src/app/page-title.ts
@@ -0,0 +1,8 @@
+
+const titleMap: Record<string, string> = {
+    "/": "Attendify",
+    "/students": "Students - Attendify",
+    "/students/list": "Students list - Attendify",
+};
+
+export default titleMap;
