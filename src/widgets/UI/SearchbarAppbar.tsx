efbef13fd3ee366dc4a1341353ecb36c39ebfb47
Ermiyas Arage <152753390+ermi111@users.noreply.github.com>
2024-08-21 12:03:25 +0300

    Add option to hide right action buttons

--- src/widgets/UI/SearchbarAppbar.tsx
+++ src/widgets/UI/SearchbarAppbar.tsx
@@ -4,14 +4,21 @@ import SearchbarAppbar from "@/components/appbars/top-appbar/searchbar/Searchbar
 import Button from "@/components/buttons/Buttons";
 import Logo from "@/components/logo/Logo";
 
-const CoreSearchbarAppbar: React.FC<{title?: React.ReactNode; hideBackBtn?: boolean}> = ({title, hideBackBtn}) => {
+interface CoreSearchbarAppbarProps {
+    title?: React.ReactNode; 
+    hideBackBtn?: boolean; 
+    hideSettingsBtn?: boolean;
+    hideNotificationsBtn?: boolean;
+}
+
+const CoreSearchbarAppbar: React.FC<CoreSearchbarAppbarProps> = ({title, hideBackBtn, hideSettingsBtn, hideNotificationsBtn}) => {
     return(
         <SearchbarAppbar
             noBackBtn={hideBackBtn}
             buttons={
                 <>
-                    <Button variant="icon" icon="settings" tooltip="Settings" href="/settings" />
-                    <Button variant="icon" icon="notifications" tooltip="Notifications" href="/notifications" />
+                    {!hideSettingsBtn && <Button variant="icon" icon="settings" tooltip="Settings" href="/settings" />}
+                    {!hideNotificationsBtn && <Button variant="icon" icon="notifications" tooltip="Notifications" href="/notifications" />}
                 </>
             }
             title={title ? title : <Logo />}
