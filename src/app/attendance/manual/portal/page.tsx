e276dcf4929141ef8e3d83cefae48a7e34010d55
Ermiyas Arage <152753390+ermi111@users.noreply.github.com>
2024-08-29 14:29:09 +0300

    Finishing front-end manaul attendance page

---
+++ src/app/attendance/manual/portal/page.tsx
@@ -0,0 +1,65 @@
+
+'use client'
+import React from "react"
+import { Accordion , AccordionItem} from "@/components/accordions/Accordions";
+import CoreSearchbarAppbar from "@/widgets/UI/SearchbarAppbar";
+import useScreenSize from "@/hooks/useScreenSize";
+import CoreNormalAppbar from "@/widgets/UI/NormalAppbar";
+import CoreNavDrawer from "@/widgets/UI/NavDrawer";
+import CoreNavbar from "@/widgets/UI/Navbar";
+import { useNavDrawer } from "@/context/NavDrawerContext";
+import Container from "@/components/containers/Containers";
+import List, { ListItem } from "@/components/lists/Lists";
+
+export default function ManualAttendancePortal() {
+	const { isNavDrawerOpen, toggleNavDrawer } = useNavDrawer();
+
+	const isLargeScreen: boolean = useScreenSize(1024);
+
+	return (
+	  	<>
+			{isLargeScreen ? <CoreNormalAppbar navdrawerOpener={toggleNavDrawer} title="Manual Attendance Portal" /> : <CoreSearchbarAppbar title="Manual Attendance Portal" />}
+			{isLargeScreen ? <CoreNavDrawer isOpen={isNavDrawerOpen} active={3} /> : <CoreNavbar active={3} />}
+            <Container hasSearchbarAppbar navDrawerOpen={isNavDrawerOpen}>
+                <Accordion variant="filled" title="Select Grade and Section">
+                    <AccordionItem label="Grade 9">
+                        <List>
+                            <ListItem href="/attendance/manual?grade=10&section=A" heading="section A" />
+                            <ListItem href="/attendance/manual?grade=10&section=A" heading="section B" />
+                            <ListItem href="/attendance/manual?grade=10&section=A" heading="section C" />
+                            <ListItem href="/attendance/manual?grade=10&section=A" heading="section D" />
+                        </List>
+                    </AccordionItem>
+                    <AccordionItem label="Grade 10">
+                        <List>
+                            <ListItem href="/attendance/manual?grade=10&section=A" heading="section A" />
+                            <ListItem href="/attendance/manual?grade=10&section=A" heading="section B" />
+                            <ListItem href="/attendance/manual?grade=10&section=A" heading="section C" />
+                            <ListItem href="/attendance/manual?grade=10&section=A" heading="section D" />
+                        </List>
+                    </AccordionItem>
+                    <AccordionItem label="Grade 11">
+                        <Accordion variant="filled">
+                            <AccordionItem label="Natural Science">
+                                <List>
+                                    <ListItem href="/attendance/manual?grade=10&section=A&&stream=Natural Science" heading="section A" />
+                                    <ListItem href="/attendance/manual?grade=10&section=A&&stream=Natural Science" heading="section B" />
+                                    <ListItem href="/attendance/manual?grade=10&section=A&&stream=Natural Science" heading="section C" />
+                                    <ListItem href="/attendance/manual?grade=10&section=A&&stream=Natural Science" heading="section D" />
+                                </List>
+                            </AccordionItem>
+                            <AccordionItem label="Social Science">
+                                <List>
+                                    <ListItem href="/attendance/manual?grade=10&section=A&&stream=Social Science" heading="section A" />
+                                    <ListItem href="/attendance/manual?grade=10&section=A&&stream=Social Science" heading="section B" />
+                                    <ListItem href="/attendance/manual?grade=10&section=A&&stream=Social Science" heading="section C" />
+                                    <ListItem href="/attendance/manual?grade=10&section=A&&stream=Social Science" heading="section D" />
+                                </List>
+                            </AccordionItem>
+                        </Accordion>
+                    </AccordionItem>
+                </Accordion>
+            </Container>
+		</>
+	);
+}
